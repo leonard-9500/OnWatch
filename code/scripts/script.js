1d7cfa74d053d4951c2325a735efd081225e04df
leonard-9500 <leonard.michel@outlook.de>
2021-08-07 10:46:46 +0200

    Implemented a button class with colors as well as text.

--- code/scripts/script.js
+++ code/scripts/script.js
@@ -1,6 +1,6 @@
 /* Program: script.js
  * Programmer: Leonard Michel
- * Start Date: 31.12.2020
+ * Start Date: 07.08.2021
  * Last Change:
  * End Date: /
  * License: /
@@ -8,3 +8,187 @@
 */
 
 /**** INITIALIZATION ****/
+
+const SCREEN_WIDTH = 1280;
+const SCREEN_HEIGHT = 720;
+
+let canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = SCREEN_WIDTH;
+canvas.height = SCREEN_HEIGHT;
+
+let mouseX = 0;
+let mouseY = 0;
+let mouseLeftPressed = false,
+    mouseRightPressed = false;
+
+let mouseLeftPressedBefore = false,
+    mouseRightPressedBefore = false;
+
+document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener("mousedown", mouseDownHandler, false);
+document.addEventListener("mouseup", mouseUpHandler, false);
+
+function mouseMoveHandler(e)
+{
+    //console.log("Mouse moved.\n");
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    //console.log("MouseX: " + mouseX + "\n" + "MouseY: " + mouseY + "\n");
+}
+
+function mouseDownHandler(e)
+{
+    //console.log("Mouse pressed.\n");
+    if (e.button == 0) { mouseLeftPressed = true; };
+    if (e.button == 2) { mouseRightPressed = true; };
+}
+
+function mouseUpHandler(e)
+{
+    //console.log("Mouse released.\n");
+    if (e.button == 0) { mouseLeftPressed = false; };
+    if (e.button == 2) { mouseRightPressed = false; };
+}
+
+class Button
+{
+	constructor()
+	{
+        this.x = 0;
+        this.y = 0;
+        this.width = 150;
+        this.height = 50;
+        // Colors
+        this.colEdgeNeutral = "#888888";
+        this.colFaceNeutral = "#00000044";
+        this.colEdgeHover = "#bbbbbb";
+        this.colFaceHover = "#00000088";
+        this.colEdgePressed = "#ffffff";
+        this.colFacePressed = "#000000bb";
+        this.colTextFill = "#000000";
+        this.colTextShadow = "#ffffff";
+        // Color assignment
+        this.edgeColor = this.colEdgeNeutral;
+        this.faceColor = this.colFaceNeutral;
+
+        this.text = "Button";
+        this.pressed = false;
+        this.visible = true;
+        // How often can the user click the button.
+        this.clickSpeed = 50;
+        this.clickTick = Date.now();
+	}
+
+    update()
+    {
+        this.collisionDetection();
+        if (this.visible)
+        {
+            this.draw();
+        }
+    }
+
+    collisionDetection()
+    {
+        // Only let the user click the button if the wait time has been passed
+        if (tp1 - this.clickTick >= this.clickSpeed)
+        {
+            // If mouse is within button bounds.
+            if (mouseX >= this.x && mouseX < this.x + this.width && mouseY >= this.y && mouseY < this.y + this.width)
+            {
+                // If mouse clicked on button
+                if (mouseLeftPressed)
+                {
+                    if (mouseLeftPressedBefore == false)
+                    {
+                        this.edgeColor = this.colEdgePressed;
+                        this.faceColor = this.colFacePressed;
+
+                        this.pressed = true;
+                        mouseLeftPressedBefore = true;
+                    }
+                }
+                // If mouse is hovering on button
+                if (!mouseLeftPressed)
+                {
+                    this.edgeColor = this.colEdgeHover;
+                    this.faceColor = this.colFaceHover;
+
+                    this.pressed = false;
+                    mouseLeftPressedBefore = false;
+                }
+            }
+            // If mouse is out of button bounds.
+            else
+            {
+                this.edgeColor = this.colEdgeNeutral;
+                this.faceColor = this.colFaceNeutral;
+
+                this.pressed = false;
+            }
+
+            this.clickTick = Date.now();
+        }
+    }
+
+    draw()
+    {
+        // Draw fill
+        ctx.fillStyle = this.faceColor;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        // Draw border
+        ctx.strokeStyle = this.edgeColor;
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+
+        // Draw text
+        let textPosX = this.x + (this.width / 2),
+            textPosY = this.y + (this.height / 1.5),
+            textSize = this.height/1.5;
+
+        ctx.textAlign = "center";
+        ctx.font = this.height / 2 + "px sans-serif";
+
+        // Text shadow
+        ctx.fillStyle = this.colTextShadow;
+        ctx.fillText(this.text, textPosX + textSize/128, textPosY + textSize/128);
+
+        // Actual text
+        ctx.fillStyle = this.colTextFill;
+        ctx.fillText(this.text, textPosX, textPosY);
+
+    }
+}
+
+let button = new Button;
+button.x = 50;
+button.y = 50;
+button.height *= 3;
+button.width *= 4;
+button.text = "Hello there.";
+
+// Time variables
+let tp1 = Date.now();
+let tp2 = Date.now();
+let elapsedTime = 0;
+
+// The game loop
+window.main = function ()
+{
+    window.requestAnimationFrame(main);
+    // Get elapsed time for last tick.
+    tp2 = Date.now();
+    elapsedTime = tp2 - tp1;
+    //console.log("elapsedTime:" + elapsedTime + "\n");
+    tp1 = tp2;
+
+    ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    button.update();
+}
+
+// Start the game loop
+main();
